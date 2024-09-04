c49ceb7fc75f71c9364aeb5f6531fde2d3e2ef8a
dtrabalho <daniellytrabalhoempresa@gmail.com>
2024-09-04 10:08:55 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('joinButton').addEventListener('click', function() {
+    window.location.href = 'https://dtrabalho.github.io/archiveeilishsbr-comunidade/';
+});
